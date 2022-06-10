f159a5386c4ebb9f4375109e145fb933f38d959a
Maryanne Anyango <mayannanneanyango98@gmail.com>
2022-06-10 16:34:41 +0300

    files

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+function saturdayFun(activityy = 'roller-skate'){
+    return "This Saturday, I want to "+activityy+"!"
+}
+
+function mondayWork(activityyy = 'go to the office'){
+    return 'This Monday, I will '+activityyy+"."
+}
+
+// const mondayWork = function (activityyy = 'go to the office') {
+//     return 'This Monday, I will '+activityyy+"."
+//   };
+
+// function wrapAdjective(){
+
+// }
+function wrapAdjective(adjective){
+    function special(receive){
+        // return `You are ${adjective}${receive}${adjective}!`
+
+        // same as 
+        
+        return `You are `+ adjective + receive + adjective+"!"
+    }
+    return special
+ }
+ wrapAdjective('*')
